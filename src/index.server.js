cfcc12cd55c1e7a35a10b582b4f5f96c3d373311
suhashebbar13 <suhashebbar13@gmail.com>
2023-02-04 15:21:38 +0530

    updated

--- src/index.server.js
+++ src/index.server.js
@@ -13,9 +13,9 @@ const productRoutes = require("./routes/product");
 const cartRoutes = require("./routes/cart");
 const initialDataRoutes = require("./routes/admin/initialData");
 const pageRoutes = require("./routes/admin/page");
-// const addressRoutes = require("./routes/address");
-// const orderRoutes = require("./routes/order");
-// const adminOrderRoute = require("./routes/admin/order.routes");
+const addressRoutes = require("./routes/address");
+const orderRoutes = require("./routes/order");
+const adminOrderRoute = require("./routes/admin/order.routes");
 
 //environment variable or you can say constants
 env.config();
@@ -46,9 +46,9 @@ app.use("/api", productRoutes);
 app.use("/api", cartRoutes);
 app.use("/api", initialDataRoutes);
 app.use("/api", pageRoutes);
-// app.use("/api", addressRoutes);
-// app.use("/api", orderRoutes);
-// app.use("/api", adminOrderRoute);
+app.use("/api", addressRoutes);
+app.use("/api", orderRoutes);
+app.use("/api", adminOrderRoute);
 
 app.listen(5000, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
