64552ef49bbcda0ddbae343445744b91ffe7d589
suhashebbar13 <suhashebbar13@gmail.com>
2023-02-05 13:40:45 +0530

    chnages made in auth controller 'SECRET'

--- src/controller/auth.js
+++ src/controller/auth.js
@@ -4,7 +4,7 @@ const bcrypt = require("bcrypt");
 const shortid = require("shortid");
 
 const generateJwtToken = (_id, role) => {
-  return jwt.sign({ _id, role }, process.env.JWT_SECRET, {
+  return jwt.sign({ _id, role }, "SECRET", {
     expiresIn: "1d",
   });
 };
